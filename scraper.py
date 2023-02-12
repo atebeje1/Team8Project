8049524b811fbf8b9168769f8b689cbd40eb63d1
atebeje1 <70766012+atebeje1@users.noreply.github.com>
2023-02-12 18:58:06 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.autoblog.com/{}-gas-prices/'.format(
-  input('Enter City, State, or Zip: '))
+type = input('What gas type do you need? Enter Regular, Mid-grade, Premium, or Diesel: ')
 
+suffix = ''
+if type == 'Regular':
+  suffix = ''
+elif type == 'Mid-grade':
+  suffix = 'premium/'
+elif type == 'Premium':
+  suffix = 'mid-grade/'
+elif type == 'Diesel':
+  suffix = 'diesel/'
+
+location = input('Enter City, State, or Zip: ')
+
+url = 'https://www.autoblog.com/{}-gas-prices/{}'.format(
+  location, suffix)
+
+print('Searching for {} gas prices in {} . . .'.format(type.casefold(), location))
 request = requests.get(url)
 content = request.content
 
@@ -15,4 +30,5 @@ data = data.find_all('li')
 
 for entry in data:
   specificdata = entry.find('ul')
+
   print(specificdata)
