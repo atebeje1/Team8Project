ba2d8c39f92eb5b84d637cc5faa115fca374f295
Ndamte <124942505+Ndamte@users.noreply.github.com>
2023-02-27 16:36:08 -0500

    Revert "Home page"

--- scripts.js
+++ scripts.js
@@ -6,3 +6,6 @@ function ExpandMenu() {
     document.getElementById("page").style.left = "25%";
     document.getElementById("menu").style.left = "0%";
 }
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
