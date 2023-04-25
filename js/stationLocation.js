2748084f9533855db1a73b7e6f4e41b8a7bba73e
Donte Robinson <dontekendallrobinson@gmail.com>
2023-04-25 15:16:13 -0400

    Updates The JS and HTML file

--- js/stationLocation.js
+++ js/stationLocation.js
@@ -1,96 +1,116 @@
 const apiKey = "AAPKd8b3d74fcd6d48c4a9d7ad7fdd2586d3uItUr-Jq29EZoumP4wUgXHzwHWOzP-nX44BOi-rGNtE1N57gh3l7nRhPI9flXWRO";
 
-      const basemapEnum = "ArcGIS:Navigation";
+const basemapEnum = "ArcGIS:Navigation";
 
-      const map = L.map("map", {
-        minZoom: 2
-      })
-      // Add a marker for the user's current location
-                function onLocationFound(e) {
-                  L.marker(e.latlng).addTo(map)
-                    .bindPopup("You are here.");
-                  var lat = e.latitude
-                  var long = e.longitude
-                  document.getElementById('output').innerHTML = lat;
-                }
-                function onLocationError(e) {
-                  alert(e.message);
-                }
-                map.on('locationfound', onLocationFound);
-                map.on('locationerror', onLocationError);
-                map.locate({setView: true, maxZoom: 16});
-      map.setView([33.753224087238785, -84.3852604445813], 14); 
+const map = L.map("map", {
+  minZoom: 2
+});
 
-      L.esri.Vector.vectorBasemapLayer(basemapEnum, {
-        apiKey: apiKey
-      }).addTo(map);
+let currentLocationMarker;
 
-      L.Control.PlacesSelect = L.Control.extend({
-        onAdd: function (map) {
+// Add a marker for the user's current location
+function onLocationFound(e) {
+  currentLocationMarker = L.marker(e.latlng).addTo(map)
+    .bindPopup("You are here.");
 
-          const placeCategories = [
-            ["", "Find:"],
-            ["Gas Station", "Gas Station"]
-          ];
+  var lat = e.latitude
+  var long = e.longitude
+  document.getElementById('output').innerHTML = lat;
+}
 
-          const select = L.DomUtil.create("select", "");
-          select.setAttribute("id", "optionsSelect");
-          select.setAttribute("style", "font-size: 16px;padding:4px 8px;");
+function onLocationError(e) {
+  alert(e.message);
+}
 
-          placeCategories.forEach((category) => {
-            let option = L.DomUtil.create("option");
-            option.value = category[0];
-            option.innerHTML = category[1];
-            select.appendChild(option);
-          });
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+map.locate({setView: true, maxZoom: 16});
+map.setView([33.753224087238785, -84.3852604445813], 14);
 
-          return select;
-        },
+L.esri.Vector.vectorBasemapLayer(basemapEnum, {
+  apiKey: apiKey
+}).addTo(map);
 
-        onRemove: function (map) {
-          // Nothing to do here
-        }
-      });
+L.Control.PlacesSelect = L.Control.extend({
+  onAdd: function (map) {
 
-      L.control.placesSelect = function (opts) {
-        return new L.Control.PlacesSelect(opts);
-      };
+    const placeCategories = [
+      ["", "Find:"],
+      ["Gas Station", "Gas Station"]
+    ];
 
-      L.control.placesSelect({
-          position: "topright"
-        }).addTo(map);
+    const select = L.DomUtil.create("select", "");
+    select.setAttribute("id", "optionsSelect");
+    select.setAttribute("style", "font-size: 16px;padding:4px 8px;");
 
-      const layerGroup = L.layerGroup().addTo(map);
+    placeCategories.forEach((category) => {
+      let option = L.DomUtil.create("option");
+      option.value = category[0];
+      option.innerHTML = category[1];
+      select.appendChild(option);
+    });
 
-      function showPlaces(category) {
+    return select;
+  },
 
-        L.esri.Geocoding
-          .geocode({
-            apikey: apiKey
-          })
-          .category(category)
-          .nearby(map.getCenter(), 10)
+  onRemove: function (map) {
+    // Nothing to do here
+  }
+});
 
-          .run(function (error, response) {
-            if (error) {
-              return;
-            }
+L.control.placesSelect = function (opts) {
+  return new L.Control.PlacesSelect(opts);
+};
 
-            layerGroup.clearLayers();
+L.control.placesSelect({
+  position: "topright"
+}).addTo(map);
 
-            response.results.forEach((searchResult) => {
-              L.marker(searchResult.latlng)
-                .addTo(layerGroup)
-                .bindPopup(`<b>${searchResult.properties.PlaceName}</b></br>${searchResult.properties.Place_addr}`);
-            });
+const layerGroup = L.layerGroup().addTo(map);
 
-          });
+let currentRouteControl;
 
+function showPlaces(category) {
+
+  L.esri.Geocoding
+    .geocode({
+      apikey: apiKey
+    })
+    .category(category)
+    .nearby(map.getCenter(), 10)
+    .run(function (error, response) {
+      if (error) {
+        return;
       }
 
-      const select = document.getElementById("optionsSelect");
-      select.addEventListener("change", () => {
-        if (select.value !== "") {
-          showPlaces(select.value);
-        }
+      layerGroup.clearLayers();
+
+      response.results.forEach((searchResult) => {
+        const destinationMarker = L.marker(searchResult.latlng)
+          .addTo(layerGroup)
+          .bindPopup(`<b>${searchResult.properties.PlaceName}</b></br>${searchResult.properties.Place_addr}`)
+          .on('click', function (e) {
+            if (currentRouteControl) {
+              map.removeControl(currentRouteControl);
+            }
+
+            currentRouteControl = L.Routing.control({
+              waypoints: [
+                currentLocationMarker.getLatLng(),
+                L.latLng(searchResult.latlng.lat, searchResult.latlng.lng)
+              ],
+              show: false
+            }).addTo(map);
+          });
       });
+
+    });
+
+}
+
+const select = document.getElementById("optionsSelect");
+select.addEventListener("change", () => {
+  if (select.value !== "") {
+    showPlaces(select.value);
+  }
+});
